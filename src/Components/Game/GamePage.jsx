6b188e9c695a6b25d5a0bfd81dca9ee2852d902d
Cole Winslow <colethomaswinslow7@gmail.com>
2023-02-09 05:02:19 -0700

    Testing squares mapping

--- src/Components/Game/GamePage.jsx
+++ src/Components/Game/GamePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function GamePage() {
-  return <div>GamePage</div>;
+  return <div className="game-box"></div>;
 }
--- src/Components/PuzzleGenerator/PuzzleEditor/SquarePuzzleMap.jsx
+++ src/Components/PuzzleGenerator/PuzzleEditor/SquarePuzzleMap.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function SquarePuzzleMap({ size }) {
-  return <div className="puzzle-map-container"></div>;
+  const [pieces, setPieces] = useState([]);
+  useEffect(() => {
+    const pieceSize = Math.sqrt((280 * 280) / size);
+    const pieces = [];
+
+    for (let i = 0; i < size; i++) {
+      const x = (i * pieceSize) % 280;
+      const y = Math.floor((i * pieceSize) / 280) * pieceSize;
+
+      pieces.push({
+        x,
+        y,
+        backgroundPosition: `-${x}px -${y}px`,
+        width: `${pieceSize}px`,
+        height: `${pieceSize}px`,
+      });
+    }
+
+    setPieces(pieces);
+  }, [size]);
+
+  return (
+    <div
+      className="puzzle-map-container"
+      style={{ display: "flex", flexWrap: "wrap" }}
+    >
+      {" "}
+      {pieces.map((piece, index) => (
+        <div className="test" key={index} style={piece} />
+      ))}
+    </div>
+  );
 }
